7aea4af928e17565399c97d6c55070805201d86b
Pushkar Sabharwal <pushkar7567@gmail.com>
2020-06-21 20:29:35 +0530

    adding random policy bot

---
+++ baselinebot.py
@@ -0,0 +1,23 @@
+import gym
+
+num_episodes = 10
+rewards = []
+
+def main():
+    env = gym.make('SpaceInvaders-v0')
+    env.reset()
+    episode_reward = 0
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        state, reward, done, info = env.step(action)
+        env.render()
+        episode_reward += reward
+        if done:
+            rewards.append(episode_reward)
+
+    print(f"Avg reward: {sum(rewards)/len(rewards)}")
+
+
+if __name__ == "__main__":
+    main()
